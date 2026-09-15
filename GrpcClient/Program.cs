a249297e3e2e90fe344b8548fa41e6aaeb267828
S7venKing <3r4n01d@gmail.com>
2026-09-15 23:30:02 +0700

    Test Grpc Client

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcLab;
+
+var channel = GrpcChannel.ForAddress("https://localhost:44371/");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "World" });
+Console.WriteLine(reply.Message);
 Console.WriteLine("Hello, World!");
