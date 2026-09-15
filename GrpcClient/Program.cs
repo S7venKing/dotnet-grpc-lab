fe3af56f2a3adb762c3ac412308394aaea1b5ebd
S7venKing <3r4n01d@gmail.com>
2026-09-15 23:38:41 +0700

    Customer Client Grpc

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -2,8 +2,13 @@
 using Grpc.Net.Client;
 using GrpcLab;
 
+//var channel = GrpcChannel.ForAddress("https://localhost:44371/");
+//var client = new Greeter.GreeterClient(channel);
+//var reply = await client.SayHelloAsync(new HelloRequest { Name = "World" });
+//Console.WriteLine(reply.Message);
+//Console.WriteLine("Hello, World!");
+
 var channel = GrpcChannel.ForAddress("https://localhost:44371/");
-var client = new Greeter.GreeterClient(channel);
-var reply = await client.SayHelloAsync(new HelloRequest { Name = "World" });
-Console.WriteLine(reply.Message);
-Console.WriteLine("Hello, World!");
+var client = new Customer.CustomerClient(channel);
+var reply = await client.GetCustomerAsync(new CustomerLookupModel { Id = 1 });
+Console.WriteLine($"Customer: {reply.Name}, Email: {reply.Email}");
